5962354cf67e2b9d0413a8f85e0cd4bb5e852c97
Charlie Van Norman <charlie@vannorman.ai>
2020-12-27 23:51:28 -0800

    added comments to deck draw calculator

--- calc.py
+++ calc.py
@@ -1,3 +1,4 @@
+#given a 60 card deck, with 4 of card X in it, how likely am I to have at least one X card in my hand after $depth draws?
 def show_chance(deck_size=60, num_in_deck=4, depth=30):
 	i = 0
 	a = 1
