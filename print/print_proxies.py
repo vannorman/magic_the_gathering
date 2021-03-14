20976c09fb2683c2f90657d4c0cc4030d4fb0ba0
Charlie Van Norman <charlie@vannorman.ai>
2021-03-14 09:19:56 -0700

    died

--- print/print_proxies.py
+++ print/print_proxies.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import sys
 from PIL import Image, ImageOps
 from urllib.request import urlopen
@@ -20,37 +21,105 @@ class bcolors:
     UNDERLINE = '\033[4m'
     NORMAL = '\033[00m'
 
+saved_cards = []
+
+def check_saved_card_exists(cardname):
+    global saved_cards
+    if len(saved_cards) == 0:
+        saved_cards = [x.strip() for x in open("saved_cards.txt",'r').readlines()]
+
+    for card in saved_cards:
+        saved_card_name = card.split(';')[0]
+        saved_card_file = card.split(';')[1]
+        if saved_card_name == cardname:
+           return saved_card_file 
+    return None
+
+def save_card_to_cache(card_name,image_file_name):
+    append_new_line("saved_cards.txt", card_name+";"+image_file_name)
+    
+
 def main(*args):
+    # make sure saved / cached cards file exists
+    Path('saved_cards.txt').touch()
+
+    # get each arg (a decklist)
     if len(sys.argv) <= 1:
         print('\n'+bcolors.WARNING+" no arg supplied"+bcolors.ENDC)
         print('\n'+bcolors.OKBLUE+"usage: "+bcolors.ENDC+"python print.py file1.txt file2.txt\n each file should be a Magic the Gathering decklist.\n\n "+bcolors.UNDERLINE+"Sample_file.txt"+bcolors.ENDC+bcolors.OKCYAN+"\n 1 Ant Queen \n 4 Sword of Fire and Ice \n 2 Rite of Replication"+bcolors.ENDC+"\n")
     for arg in sys.argv[1:]:
         print("arg:"+arg)
     for filename in sys.argv[1:]:
+        # for each decklist,
         master_list = {}    
 
         cards = [x.strip() for x in open(filename,'r').readlines()]
+        
+        # for each card in that decklist,
         for card in cards:
             if len(card) == 0 or not card[0].isnumeric():
                 print(bcolors.WARNING+"SKIP "+bcolors.ENDC+"card, no num")
                 continue
-            # separate number from search
-            count, search_term = int(card[:1]), card[2:]
-        
-            # use scryfall opensearch from opensearch.xml on scryfall.com source
-            html = urlopen('https://scryfall.com/search?q='+search_term.replace(' ',''))
-    #       print(str(html)[:40])
-            bs = BeautifulSoup(html,'html.parser')
-            image_url = bs.find_all('img')[0]['src']
-            image_file_name = re.search('([^\/]+$)',image_url)[0] # after last "/"
-            image_file_name = re.search('([^?]*)', image_file_name)[0] # before "?"
-            if path.exists('images/'+image_file_name):
-                print(bcolors.OKCYAN+'image '+image_file_name+' exists, skipping!')
-            else:
-                print(bcolors.OKBLUE+'adding:'+image_file_name)
-                urllib.request.urlretrieve(image_url,'images/'+image_file_name)
+    
+            # separate number from search, e.g. "4 Angel of Avacyn" becomes "Angel of Avacyn"
+            count, card_name = int(card[:1]), card[2:]
 
-            print(bcolors.ENDC)
+            # Did we already download this card before?
+            saved_card_file_name = check_saved_card_exists(card_name)
+            image_file_name = None
+
+            # if so, read the file name from the saved_cards.txt and don't bother downloading it
+            if saved_card_file_name is not None: 
+                print(bcolors.OKBLUE+'card '+card_name+' was saved! ('+saved_card_file_name+') -- skip check')
+                print(bcolors.ENDC)
+                image_file_name = saved_card_file_name
+            
+            # Nope I've never seen this card before in my life. Download it
+            else:
+                # use scryfall opensearch from opensearch.xml on scryfall.com source
+                html = urlopen('https://scryfall.com/search?q='+card_name.replace(' ','')+'&unique=cards&as=grid&order=usd&dir=desc')
+                bs = BeautifulSoup(html,'html.parser')
+
+                # here, it's possible we found multiple cards.
+                # if so, the first one is usually correct (shortest name).
+                if bs.find_all("a", {"class": "card-grid-item-card"}):
+                    print(bcolors.OKBLUE+'found card grid item:')
+                    for item in  bs.find_all("a", {"class": "card-grid-item-card"}):
+                        print('item:'+str(item))
+                    card_link = bs.find_all("a", {"class": "card-grid-item-card"})[0]['href']
+                    print(' and card link:'+card_link)
+                    html = urlopen(card_link)
+                    bs = BeautifulSoup(html,'html.parser')
+                    image_url = bs.find_all('img')[0]['src']
+                    print('.. and img url:'+image_url)
+                    print(bcolors.ENDC)
+                
+                 
+
+            
+                image_url = bs.find_all('img')[0]['src']
+                print('.. then img url:'+image_url)
+                image_file_name = re.search('([^\/]+$)',image_url)[0] # after last "/"
+                image_file_name = re.search('([^?]*)', image_file_name)[0] # before "?"
+                if path.exists('images/'+image_file_name):
+                    print(bcolors.OKCYAN+'image '+image_file_name+' exists, skipping!')
+                else:
+                    print(bcolors.OKBLUE+'adding:'+image_file_name)
+                    urllib.request.urlretrieve(image_url,'images/'+image_file_name)
+
+                print(bcolors.ENDC)
+    
+                # We downloaded a card. Now save the card! For later. :-]
+                save_card_to_cache(card_name,image_file_name)
+
+            # Now, we have the card image
+            # but sometimes card image is wrong size
+            # let's make sure its right size, 672x936
+
+#            im = Image.open('images/'+image_file_name)
+#            im = im.resize((672, 936))
+#            im.save('images/'+image_file_name)
+
             master_list['images/'+image_file_name] = count
         
         print(master_list)
@@ -111,7 +180,7 @@ def main(*args):
     
         
 def combine(image_files = ['a.png' for i in range(9)]):
-    # print('\n combine starting with:\n'+str(image_files)+'\n')
+#    print('\n combine starting with:\n'+str(image_files)+'\n')
     images = [Image.open(x) for x in image_files]
 
     c1 = 9 # crop border size
@@ -150,6 +219,21 @@ def clean_temp_dir():
                 shutil.rmtree(file_path)
         except Exception as e:
             print('Failed to delete %s. Reason: %s' % (file_path, e))
+
+def append_new_line(file_name, text_to_append):
+    """Append given text as a new line at the end of file"""
+    # Open the file in append & read mode ('a+')
+    with open(file_name, "a+") as file_object:
+        # Move read cursor to the start of file.
+        file_object.seek(0)
+        # If file is not empty then append '\n'
+        data = file_object.read(100)
+        if len(data) > 0:
+            file_object.write("\n")
+        # Append text at the end of file
+        file_object.write(text_to_append)
+
+
 
 if __name__ == "__main__":
     # execute only if run as a script
